9a3c26079a191e562d091661941aa4c0856e688b
Anm Abrar <17101013@uap-bd.edu>
2022-03-18 15:53:00 +0600

    banner aded

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -3,11 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  i : any = "https://www.cleanmoswabs.com/photo/cs33440483-disposable_sterile_swab.jpg"
+  j : any = "https://www.cleanmoswabs.com/photo/cs33938765-chg_swab.jpg"
+  k : any = "https://www.cleanmoswabs.com/photo/cs33440483-disposable_sterile_swab.jpg"
+
+  images = [this.i, this.j, this.k].map((n) => `${n}`);
 
   ngOnInit(): void {
   }
