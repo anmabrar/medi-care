c3facc743c3a03016635881816b416a6ace8eafc
Anm Abrar <17101013@uap-bd.edu>
2022-03-30 02:13:58 +0600

    Dashboard added

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  display : any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addDoctor(){
+    this.display ="doctor"
+  }
+
+  addMedi(){
+    this.display = "medi"
+  }
+
+  sendReview(){
+    this.display = "review"
+  }
+
+  doctorList(){
+    this.display = "doctorList"
   }
 
 }
